cc6fe621ff0fe87e26da81338a52641f85ccf438
mygoaway <mygoaway2@gmail.com>
2021-07-09 23:53:35 +0900

    7월9일 commit

---
+++ algorithm/src/algorithm/study210709/SingleNumber.java
@@ -0,0 +1,26 @@
+package algorithm.study210709;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SingleNumber {
+	
+	public int singleNumber(int[] nums) {
+	    List<Integer> list = new ArrayList<>();
+
+	    for (int i : nums) {
+	      if (!list.contains(i)) {
+	        list.add(i);
+	      } else {
+	        list.remove(new Integer(i));
+	      }
+	    }
+	    return list.get(0);
+	  }
+	
+    public static void main(String[] args) {
+    	SingleNumber sn = new SingleNumber();
+    	int result = sn.singleNumber(new int[] {2,4,2,4,1});
+       System.out.println((result));
+    }
+}
