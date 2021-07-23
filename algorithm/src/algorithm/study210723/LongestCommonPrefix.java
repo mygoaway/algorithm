41d3fd16edee95b183b0bc0a986be039b3ec36cd
mygoaway <mygoaway2@gmail.com>
2021-07-23 22:53:14 +0900

    2번 commit

---
+++ algorithm/src/algorithm/study210723/LongestCommonPrefix.java
@@ -0,0 +1,34 @@
+package algorithm.study210723;
+
+import java.util.Stack;
+
+public class LongestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+        int minSize = 100;
+        int idx = -1;
+        
+        for(int i = 0; i < strs.length; i++){
+            if(strs[i].length() < minSize){
+            	minSize = strs[i].length();
+                idx = i;
+            }
+        }
+        String s = strs[idx];
+        
+        for(int j = 0; j < minSize; j++){
+            for(int k = 0; k < strs.length; k++){
+                if(s.charAt(j) != strs[k].charAt(j)){
+                    return s.substring(0,j);
+                }
+            }
+        }
+        return s;
+    }
+
+	
+    public static void main(String[] args) {
+    	LongestCommonPrefix lcp = new LongestCommonPrefix();
+    	String result = lcp.longestCommonPrefix(new String [] {"jay", "japt", "japa"});
+        System.out.println((result));
+    }
+}
