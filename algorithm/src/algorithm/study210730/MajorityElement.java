df8a8b9a1f0051d41a8266d50a1f8b122b6344b0
mygoaway <mygoaway2@gmail.com>
2021-07-30 23:19:09 +0900

    2번 commit

---
+++ algorithm/src/algorithm/study210730/MajorityElement.java
@@ -0,0 +1,27 @@
+package algorithm.study210730;
+
+public class MajorityElement {
+    public int majorityElement(int [] nums) {
+    	int majority =nums[0];
+    	int counter = 1;
+    	for(int i=1;i<nums.length;i++){
+	    	if(majority == nums[i]){
+	    		counter ++;
+	    	} else {
+	    		counter --;
+	    		if(counter == 0){
+	    			majority = nums[i];
+	    			counter = 1;
+	    		}
+	    	}
+    	}
+    	return majority;
+    }
+
+	
+    public static void main(String[] args) {
+    	MajorityElement lcp = new MajorityElement();
+    	int result = lcp.majorityElement(new int [] {3,2,3,2,3,3});
+        System.out.println((result));
+    }
+}
