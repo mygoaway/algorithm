a5759c7b620725e071473742822afe8e5b9084c0
mygoaway <mygoaway2@gmail.com>
2021-08-02 22:20:30 +0900

    commit

--- algorithm/src/algorithm/study210730/MajorityElement.java
+++ algorithm/src/algorithm/study210730/MajorityElement.java
@@ -4,7 +4,6 @@ public class MajorityElement {
     public int majorityElement(int [] nums) {
     	int majority =nums[0];
     	int counter = 1;
-    	
     	for(int i=1;i<nums.length;i++){
 	    	if(majority == nums[i]){
 	    		counter ++;
